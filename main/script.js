65a7fe329dd78a79e5175e0c4baf421ef1511a54
LeuhanVFX <leuhan.vfx@gmail.com>
2026-03-13 11:27:17 +0100

    ça marche enfin

--- main/script.js
+++ main/script.js
@@ -100,30 +100,6 @@ window.addEventListener('scroll', () => {
 });
 
 /// Menu Mobile
-// let burger_button = document.querySelector('.burger .hamburger input');
-// let mobile_menu = document.getElementById('mobile-menu');
-
-// burger_button.addEventListener('change', () => {
-//     if (burger_button.checked) {
-//         mobile_menu.style.visibility = 'visible';
-//         mobile_menu.style.transform = 'translateY(0)';
-//         mobile_menu.style.opacity = '1';
-//         mobile_menu.style.position = 'relative'
-//         mobile_menu.addEventListener('click', () => {
-//             burger_button.checked = false
-//             mobile_menu.style.visibility = 'hidden';
-//             mobile_menu.style.transform = 'translateY(-10px)';
-//             mobile_menu.style.opacity = '0';
-//             mobile_menu.style.position = 'absolute';
-//         })
-//     } else {
-//         mobile_menu.style.visibility = 'hidden';
-//         mobile_menu.style.transform = 'translateY(-10px)';
-//         mobile_menu.style.opacity = '0';
-//         mobile_menu.style.position = 'absolute';
-//     }
-// })
-
 
 const burgerCheck = document.getElementById('burger-check');
 const mobileMenu = document.getElementById('mobile-menu');
